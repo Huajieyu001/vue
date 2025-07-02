a256f70de01f837010a676ae59aad139537b5161
Huajieyu001 <331173500@qq.com>
2025-07-02 23:27:09 +0800

    0702

---
+++ vue_test/src05-scoped/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <Student/>
+        <School/>
+    </div>  
+</template>
+
+<script lang='js'>
+    import School from './components/School'
+    import Student from './components/Student'
+    export default {
+        name:'App',
+        components:{
+            Student,
+            School
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_test/src05-scoped/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 class="test">my name is :{{name}}</h1>
+        <h1 class="test">my age is :{{age}}</h1>
+    </div>
+</template>
+
+<script lang=js>
+    export default {
+        name:'Student',
+        data(){
+            return {
+                name:'Tom',
+                age: 18
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .test{
+        background-color: orange;
+    }
+</style>
