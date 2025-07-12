73d8153157a9221b673790536100139eb2514642
Huajieyu001 <331173500@qq.com>
2025-07-12 23:10:25 +0800

    0712

--- vue_test/src/components/Todo.vue
+++ vue_test/src/components/Todo.vue
@@ -1,28 +1,53 @@
 <template>
-  <div>
+  <li>
     <label>
-        <input type='checkbox' :checked="todo.checked">
+        <!-- <input type='checkbox' :checked="todo.checked" @click="todo.checked=!todo.checked"> -->
+        <input type='checkbox' placeholder="请输入项目" :checked="todo.checked" @change="changeStatus(todo.id)">
+        <!-- <input type='checkbox' v-model="todo.checked"> -->
         <span>{{todo.title}}</span>
     </label>
-    <button @click="del" class='btn'>delete</button>
-  </div>
+    <button @click="del(todo.id)" class='todoBtn'>delete</button>
+  </li>
 </template>
 
 <script>
 export default {
-    props:{
-        todo:{
-            type:Object
-        }
-    },
+    props:['todo','changeStatus','deleteTodo'],
     methods:{
-        del(){
-            this.todo.checked = false;
+        del(id){
+            if(confirm('确定删除？')){
+                console.log(id)
+                this.deleteTodo(id)
+            }
         }
     }
 }
 </script>
 
 <style>
+    li{
+        height: 30px;
+        width: 480px;
+        /* margin: 10px 0px; */
+        border: solid 1px;
+        padding: 0px 5px;
+    }
+    li:hover{
+        background-color: rgb(162, 168, 162);
+    }
 
+    li:hover button{
+        height: 20px;
+        width: 60px;
+        margin-left: 10px;
+        margin-right: 10px;
+        display: block;
+    }
+    .todoBtn{
+        height: 20px;
+        width: 60px;
+        margin-left: 10px;
+        margin-right: 10px;
+        display: none;
+    }
 </style>
