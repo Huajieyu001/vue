8e3bdcf760d7bf49ba86b491e5e01a1f092e614f
Huajieyu001 <331173500@qq.com>
2025-07-16 23:00:40 +0800

    0716

---
+++ vue_test/src07-emit/components/School.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="s1">
+    <h1>{{name}}</h1>
+    <h1>{{buildDate}}</h1>
+    <button @click="sendSchoolName(name)">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data(){
+        return {
+            name: 'Uni',
+            buildDate: 1930
+        }
+    },
+    props:['sendSchoolName']
+}
+</script>
+
+<style scoped>
+    .s1{
+        border: solid 0.5px;
+        padding: 5px;
+        background-color: gray;
+    }
+</style>
