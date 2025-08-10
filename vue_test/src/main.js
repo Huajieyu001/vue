46fb0c5529a803e2731b32178227e2999fb5a3ad
Huajieyu001 <331173500@qq.com>
2025-08-10 23:38:23 +0800

    0810

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+// import Vuex from 'vuex'
+import store from './store/index'
 
 Vue.config.productionTip=false
 
 Vue.use(VueResource)
+// Vue.use(Vuex)
 
-new Vue({
+const vm = new Vue({
         el:'#app',
         render(create){
             return create(App)
         },
+        store,
         beforeCreate(){
             Vue.prototype.$bus = this
         }
     },
 )
+
+console.log(vm)
