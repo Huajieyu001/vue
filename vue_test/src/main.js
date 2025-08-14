cab958a58efe76ed146d19853ef62b768956a3d1
Huajieyu001 <331173500@qq.com>
2025-08-14 23:34:17 +0800

    0814

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import router from './router'
+// import { ElRow } from 'element-ui/types/row'
+// import { ElDatePicker } from 'element-ui/types/date-picker'
+// import { from } from 'core-js/core/array'
+// 完整引入El UI
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+// 按需引入
+import {Button, Row, DatePicker} from 'element-ui'
 
 Vue.config.productionTip=false
 
-Vue.use(VueRouter)
+Vue.component(Button.name, Button)
+Vue.component(Row.name, Row)
+Vue.component(DatePicker.name, DatePicker)
 
 const vm = new Vue({
         el:'#app',
         render(create){
             return create(App)
-        },
-        router
+        }
     },
 )
